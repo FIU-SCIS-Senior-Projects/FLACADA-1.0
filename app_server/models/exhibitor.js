8e23210ab16058ba97fc63dcde80129ac8a4cb37
mabela16 <mamay005@fiu.edu>
2017-06-19 18:41:53 -0400

    done with models for sessions and speakers

--- app_server/models/exhibitor.js
+++ app_server/models/exhibitor.js
@@ -1,10 +1,22 @@
 var mongoose = require('mongoose');
 
 var exhibitorSchema = new mongoose.Schema({
-	nameOf: {type: String, required: true},
-	imageOf: {type: String, required: true},
-	website: {type: String, required: true},
-	description: {type: String, required: true}
+	name: {
+		type: String, 
+		required: true
+	},
+	image: {
+		type: String, 
+		//required: true
+	},
+	website: {
+		type: String, 
+		//required: true
+	},
+	description: {
+		type: String, 
+		required: true
+	}
 });
 
-mongoose.model('Exhibitor', exhibitorSchema);
+mongoose.model('Exhibitor', exhibitorSchema, 'exhibitors');
