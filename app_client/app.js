79616f89580a1e9cac7f1462e3e719f6eee5ec54
mabela16 <mamay005@fiu.edu>
2017-06-27 20:48:40 -0400

    added routing for app angular

--- app_client/app.js
+++ app_client/app.js
@@ -1,30 +1,40 @@
 (function () {
 
-  angular.module('flacadaApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+    angular.module('flacadaApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
-  function config($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '/home/home.view.html',
-        controller: 'homeCtrl',
-        controllerAs: 'vm'
-      })
-       .when('/exhibitors', {
-        templateUrl: '/locationDetail/locationDetail.view.html',
-        controller: 'locationDetailCtrl',
-        controllerAs: 'vm'
-      })
-      .when('/exhibitors/:exhibitorid', {
-        templateUrl: '/locationDetail/locationDetail.view.html',
-        controller: 'locationDetailCtrl',
-        controllerAs: 'vm'
-      })
-      .otherwise({ redirectTo: '/' });
-      $locationProvider.html5Mode(true);
-  }
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/home/home.view.html',
+                controller: 'homeCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/exhibitors', {
+                templateUrl: '/exhibitors/exhibitors.view.html',
+                controller: 'exhibitorsCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/exhibitors/:exhibitorid', {
+                templateUrl: '/details/exhibitorDetail.view.html',
+                controller: 'exhibitorDetailCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/sponsors', {
+                templateUrl: '/sponsors/sponsors.view.html',
+                controller: 'sponsorsCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/sponsors/:sponsorid', {
+                templateUrl: '/details/sponsorDetail.view.html',
+                controller: 'sponsorDetailCtrl',
+                controllerAs: 'vm'
+            })
+            .otherwise({ redirectTo: '/' });
+        $locationProvider.html5Mode(true);
+    }
 
-  angular
-    .module('loc8rApp')
-    .config(['$routeProvider', '$locationProvider', config]);
+    angular
+        .module('flacadaApp')
+        .config(['$routeProvider', '$locationProvider', config]);
 
 })();
