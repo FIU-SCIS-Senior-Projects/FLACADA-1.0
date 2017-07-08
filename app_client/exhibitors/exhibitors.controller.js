7117efe463ab45dec9892bbbdde71f57bd21b0f8
System Administrator <root@Nickolass-MacBook-Pro.local>
2017-07-08 16:36:34 -0400

    task-711 done

--- app_client/exhibitors/exhibitors.controller.js
+++ app_client/exhibitors/exhibitors.controller.js
@@ -4,8 +4,8 @@
         .module('flacadaApp')
         .controller('exhibitorsCtrl', exhibitorsCtrl);
 
-    exhibitorsCtrl.$inject = ['$scope', 'flacadaData'];
-    function exhibitorsCtrl($scope, flacadaData) {
+    exhibitorsCtrl.$inject = ['$scope', 'flacadaData', '$uibModal'];
+    function exhibitorsCtrl($scope, flacadaData, $uibModal) {
         var vm = this;
         vm.pageHeader = 'Exhibitors';
 
@@ -18,6 +18,17 @@
             .error(function (e) {
                 console.log(e);
             });
+
+        vm.popupExhibitorForm = function () {
+            var uibModal = $uibModal.open({
+                templateUrl: '/exhibitorModal/exhibitorModal.view.html',
+                controller: 'exhibitorModalCtrl as vm'
+            });
+
+            uibModal.result.then(function (data) {
+                vm.data.exhibitors.push(data);
+            })
+        }
 
     }
 
