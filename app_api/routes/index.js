9cfb1778775334ee0430a5bf2f6af2ddcda6610f
mabela16 <mamay005@fiu.edu>
2017-07-09 16:23:40 -0400

    added auth to api routes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -14,32 +14,32 @@ var ctrlAuth = require('../controllers/authentication');
 
 /* Exhibitors */
 router.get('/exhibitors', ctrlExhibitor.getExhibitors);
-router.post('/exhibitors', ctrlExhibitor.createExhibitor);
-router.delete('/exhibitors', ctrlExhibitor.deleteExhibitors);
+router.post('/exhibitors', auth, ctrlExhibitor.createExhibitor);
+router.delete('/exhibitors', auth, ctrlExhibitor.deleteExhibitors);
 router.get('/exhibitors/:exhibitorid', ctrlExhibitor.getOneExhibitor);
-router.delete('/exhibitors/:exhibitorid', ctrlExhibitor.deleteOneExhibitor);
+router.delete('/exhibitors/:exhibitorid', auth, ctrlExhibitor.deleteOneExhibitor);
 
 /* Sponsors */ 
 router.get('/sponsors', ctrlSponsor.getSponsors);
-router.post('/sponsors', ctrlSponsor.createSponsor);
-router.delete('/sponsors', ctrlSponsor.deleteSponsors);
+router.post('/sponsors', auth,  ctrlSponsor.createSponsor);
+router.delete('/sponsors', auth, ctrlSponsor.deleteSponsors);
 router.get('/sponsors/:sponsorid', ctrlSponsor.getOneSponsor);
-router.delete('/sponsors/:sponsorid', ctrlSponsor.deleteOneSponsor);
+router.delete('/sponsors/:sponsorid', auth, ctrlSponsor.deleteOneSponsor);
 
 /*Sessions */
 router.get('/sessions', ctrlSession.getSessions);
-router.post('/sessions', ctrlSession.createSession);
-router.delete('/sessions', ctrlSession.deleteSessions);
+router.post('/sessions', auth, ctrlSession.createSession);
+router.delete('/sessions', auth, ctrlSession.deleteSessions);
 router.get('/sessions/:sessionid', ctrlSession.getOneSession);
-router.delete('/sessions/:sessionid', ctrlSession.deleteOneSession);
-router.post('/sessions/:sessionid/speakers', ctrlSession.createSessionSpeaker); // add speaker to a session
+router.delete('/sessions/:sessionid', auth, ctrlSession.deleteOneSession);
+router.post('/sessions/:sessionid/speakers', auth, ctrlSession.createSessionSpeaker); // add speaker to a session
 
 /** Speakers */
 router.get('/speakers', ctrlSpeaker.getSpeakers);
-router.delete('/speakers', ctrlSpeaker.deleteSpeakers);
+router.delete('/speakers', auth, ctrlSpeaker.deleteSpeakers);
 router.get('/speakers/:speakerid', ctrlSpeaker.getOneSpeaker);
-router.delete('/speakers/:speakerid', ctrlSpeaker.deleteOneSpeaker);
-router.post('/speakers/:speakerid/sessions', ctrlSpeaker.createSpeakerSession); //add session to a speaker
+router.delete('/speakers/:speakerid', auth, ctrlSpeaker.deleteOneSpeaker);
+router.post('/speakers/:speakerid/sessions', auth, ctrlSpeaker.createSpeakerSession); //add session to a speaker
 
 //login and register
 router.post('/register', ctrlAuth.register);
