964201666b3ff6004f0a2e69fef5db1666930a94
Nickolas morales <ongoingbroom70@nickolass-mbp.lan>
2017-07-09 00:41:04 -0400

    task-732 done

---
+++ app_client/common/services/authenticaion.service.js
@@ -0,0 +1,67 @@
+/**
+ * Created by ongoingbroom70 on 7/8/17.
+ */
+(function () {
+    angular
+        .module('flacadaApp')
+        .service('authentication', authentication);
+    authentication.$inject = ['$window'];
+    function authentication($window) {
+
+        var saveToken = function (token) {
+            $window.localStorage['flacada-token'] = token;
+        };
+
+        var getToken = function () {
+            return $window.localStorage['flacada-token'];
+        };
+
+        register(function (user) {
+            return $http.post('/api/register', user).success(function (data) {
+                saveToken(data.token);
+            });
+        });
+
+        login = function (user) {
+            return $http.post('/api/login', user).success(function (data) {
+                saveToken(data.token);
+            })
+        };
+
+        logout = function () {
+            $window.localStorage.removeItem('flacada-token');
+        };
+
+        var isLoggedIn = function () {
+            var token = getToken();
+
+            if(token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        var currentUser = function () {
+            if(isLoggedIn()) {
+                var token = getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+                return {
+                    email : payload.email,
+                    name : payload.name
+                };
+            }
+         };
+
+        return {
+            saveToken : saveToken,
+            getToken : getToken,
+            register : register,
+            logout : logout,
+            isLoggedIn : isLoggeIn,
+            currentUser : currentUser
+        }
+    }
+}) ();
