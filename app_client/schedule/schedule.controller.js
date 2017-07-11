7e5d9f0fa9b0b4224ceeb545bae41852c2c19ffa
Nickolas morales <ongoingbroom70@nickolass-mbp.lan>
2017-07-10 22:39:33 -0400

    Undef URL

--- app_client/schedule/schedule.controller.js
+++ app_client/schedule/schedule.controller.js
@@ -4,12 +4,15 @@
         .module('flacadaApp')
         .controller('scheduleCtrl', scheduleCtrl);
 
-    scheduleCtrl.$inject = ['$scope', 'flacadaData', 'authentication'];
-    function scheduleCtrl($scope, flacadaData, authentication) {
+    scheduleCtrl.$inject = ['$location','flacadaData', 'authentication','$uibModal', '$routeParams'];
+    function scheduleCtrl($location,flacadaData, authentication, $uibModal, $routeParams) {
         var vm = this;
         vm.pageHeader = 'Schedule';
         vm.isAdmin = authentication.isAdmin();
         vm.isLoggedIn = authentication.isLoggedIn();
+        vm.sessionid - $routeParams.sessionid;
+        vm.currentPath = $location.path();
+        vm.sessionid = $routeParams.sessionid;
 
         flacadaData.sessions()
             .success(function (data) {
@@ -19,6 +22,25 @@
             .error(function (e) {
                 console.log(e);
             });
+
+        vm.addSpeakerForm= function() {
+            var uibModal = $uibModal.open({
+                templateUrl: '/speakerModal/speakerModal.view.html',
+                controller: 'speakerModalCtrl as vm',
+                resolve : {
+                    sessionData : function () {
+                        return {
+                            sessionid : vm.sessionid
+                        };
+                    }
+                }
+            })
+
+            // TODO: SPEAKER.SESSIONS OR SESSION.SPEAKER TRIED BOTH
+            uibModal.result.then(function (data) {
+                vm.data.speaker.sessions.push(data);
+            })
+        };
 
         vm.delete = function (sessionid) {
             console.log(sessionid);
